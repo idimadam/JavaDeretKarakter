fac63d3303914d1f4c4a37fc4e0ce552bad36ea0
idimadam <imamab7@gmail.com>
2016-11-04 15:39:31 +0700

    12141371 Penggunaan JOptionPane.showMessageDialog untuk menampilkan informasi atau sesuatu kepada pengguna apa yang telah terjadi. penggunaan JOptionPane.showInputDialog untuk pengguna memasukkan inputan. sedangkan ParseInt digunakan untuk merubah tipe String ke Integer

--- src/javaderetkarakter/JavaDeretKarakter.java
+++ src/javaderetkarakter/JavaDeretKarakter.java
@@ -18,3 +18,10 @@ public class Tugas1Segitiga {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Selamat Datang\nUntuk memilih Bentuk Karakter, Silahkan pilih salah satu dari daftar berikut");
+        String pil = JOptionPane.showInputDialog("1.Segitiga Siku Kiri Atas\n2.Segitiga Siku Kiri Bawah\n"
+                + "3.Segitiga Siku Kanan Atas\n4.Segitiga Siku Kanan Bawah\n5.Segitiga Sama Kaki");
+        int pilihan = Integer.parseInt(pil);
+        
+        String tinggi = JOptionPane.showInputDialog("Masukkan karakter : ");
+        int xtinggi = Integer.parseInt(tinggi);
